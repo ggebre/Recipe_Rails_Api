12092403f82a3407ad805f308359e162f2f9ef90
Getu Gebre <getu.gebre.gg@gmail.com>
2021-04-28 15:43:14 -0500

    update ruby version

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,6 @@
 class RecipesController < ApplicationController
   before_action :set_recipe, only: [:show, :update, :destroy]
-  before_action :authorized
+  # before_action :authorized
   # GET /recipes
   def index
     @recipes = Recipe.all
